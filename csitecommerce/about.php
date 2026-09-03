2cf4767db8f67e1f41783094168532b97d0d794c
BLIGHT <shaming000731@gmail.com>
2026-09-03 22:38:48 +0545

    Add files via upload

--- csitecommerce/about.php
+++ csitecommerce/about.php
@@ -3,7 +3,9 @@ require_once "header.php";
 
 ?>
 
-<h1>about us</h1>
+<h1>
+    lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</h1>
 
 <?php
 require_once "footer.php";
